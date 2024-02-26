bc5ac0f859b984850b29babc4b58b5fe52eda579
ReneRamosTrvn <reneramostrvn@gmail.com>
2024-02-26 00:24:32 -0600

    Validations on finder, removed default about us page

--- src/components/Finder.vue
+++ src/components/Finder.vue
@@ -17,17 +17,45 @@
           <label class="w-full font-semibold" for="location"
             >Pick-up date</label
           >
-          <VueDatePicker class="" v-model="pickupDate" />
+          <VueDatePicker
+            class=""
+            v-model="pickupDate"
+            disable-year-select
+            time-picker-inline
+            :start-time="startTime"
+            :enable-time-picker="false"
+            :min-date="new Date()"
+            auto-apply
+            :month-change-on-scroll="false"
+          />
         </div>
         <div class="w-full lg:w-1/4 px-2 py-5 lg:py-0">
           <label class="w-full font-semibold" for="location">Return date</label>
-          <VueDatePicker v-model="returnDate" />
+          <VueDatePicker
+            v-model="returnDate"
+            disable-year-select
+            time-picker-inline
+            :start-time="startTime"
+            :enable-time-picker="false"
+            :min-date="pickupDate"
+            auto-apply
+            :month-change-on-scroll="false"
+          />
         </div>
         <div
           class="w-full lg:w-1/4 text-center py-5 lg:py-0 flex items-center justify-center"
         >
+          <router-link
+            v-if="pickupDate && returnDate"
+            class="w-2/3 px-5 py-2 bg-red-700 hover:bg-red-600 text-white font-semibold rounded-full"
+            to="/cars"
+          >
+            <button>Search</button>
+          </router-link>
           <button
-            class="w-2/3 px-5 py-2 bg-red-700 text-white font-semibold rounded-full hover:bg-red-500"
+            v-if="!pickupDate || !returnDate"
+            :disabled="!pickupDate || !returnDate"
+            class="w-2/3 px-5 py-2 bg-gray-700 text-white font-semibold rounded-full"
           >
             Search
           </button>
@@ -38,11 +66,18 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
+import { RouterLink } from "vue-router";
 
-const pickupDate = ref();
-const returnDate = ref();
+const pickupDate = ref(null);
+const returnDate = ref(null);
+const startTime = ref({ hours: 0, minutes: 0 });
 
+onMounted(() => {});
+
+function validateReturnDate() {
+  console.log(returnDate);
+}
 const storeLocations = [
   "Dallas, Texas",
   "Mesquite, Texas",
